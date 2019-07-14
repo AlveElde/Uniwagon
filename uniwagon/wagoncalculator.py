331b41542b170e7f3cc7bd97d60627f815387545
Alve Elde <alve_elde@hotmail.com>
2019-07-14 16:34:37 +0200

    Implemented recursive create_item function

--- uniwagon/wagoncalculator.py
+++ uniwagon/wagoncalculator.py
@@ -8,7 +8,7 @@ class Item:
     name = ""
     time = 0
     stack = 0
-    materials = []
+    materials = {}
 
     def __init__(self, name):
         self.name = name
@@ -20,23 +20,32 @@ class Item:
             print("Amount:   %s" % _material[1])
             
 
-def create_item_recipe(item_name):
+def create_item(item_name, item_dict):
+    # Check if the item has already been created.
+    if item_dict.get(item_name) is not None:
+        return True
+
+     # Get the item recipe.
     _recipe = recipes.get(item_name)
     if _recipe is None:
-        print("Found no recipe for: ", item_name)
-        return None
+        return True
 
+    # Get the materials required for the normal recipe.
     _materials = _recipe.get("recipe")
     if _materials is None:
-        print("No \"recipe\" key found for: ", item_name)
-        return None
+        print("File format error: No \"recipe\" key found for: ", item_name)
+        return False
     
+    # Create new item and insert in the dict.
     _item = Item(item_name)
     _item.time = _materials[0]
+    item_dict[item_name] = _item
+
+    # Create items for each material.
     for _material in _materials[1:]:
-        _item.materials.append(_material)
-    
-    return _item
+        if not create_item(_material[0], item_dict):
+            return False
+    return True
     
 
 def main(as_module=False):
@@ -44,9 +53,19 @@ def main(as_module=False):
         print("Error: No recipe name provided")
         return
 
+    # Create item structure from name argument.
     _item_name = str(sys.argv[1])
-    _item = create_item_recipe(_item_name)
-    _item.print_materials()
+    _item_dict = {}
+    if not create_item(_item_name, _item_dict):
+        return
+
+    # Get the requested item.
+    _item = _item_dict.get(_item_name)
+    if _item is None:
+        print("Argument error: No recipe entry found for: ", _item_name)
+        return
+    
+    print("Recipe created for: ", _item.name)
 
 if __name__ == "__main__":
     main(as_module=True)
