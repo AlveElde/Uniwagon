7df5e65fef0dff3c000844bc8e5d3f25e65c2da3
Alve Elde <alve_elde@hotmail.com>
2019-07-14 15:13:09 +0200

    Safer json parsing

--- uniwagon/wagoncalculator.py
+++ uniwagon/wagoncalculator.py
@@ -4,16 +4,24 @@ import json
 with open("data/wiki-recipes-0.17.52.json", "r") as recipe_file:
     recipes = json.load(recipe_file)
 
+
 def print_recipe(item_name):
-    print("Looking for: ", item_name)
-    for _recipe_name in recipes:
-        if _recipe_name == item_name:
-            _recipe = recipes[_recipe_name]
-            print("Found recipe: %s" % _recipe_name)
-            for _ingredient in _recipe["recipe"]:
-                print("Ingredient: %s" % _ingredient[0])
-                print("Amount: %s" % _ingredient[1])
-                print("----------")
+    _recipe = recipes.get(item_name)
+    if not _recipe:
+        print("Found no recipe for: ", item_name)
+        return
+
+    _ingredients = _recipe.get("recipe")
+    if not _ingredients:
+        print("No \"recipe\" key found for: ", item_name)
+        return
+
+    print("Found recipe for: %s" % item_name)
+    for _ingredient in _ingredients:
+        print("Ingredient: %s" % _ingredient[0])
+        print("Amount: %s" % _ingredient[1])
+        print("----------")
+
 
 
 def main(as_module=False):
