d85a2cb1b98d5fe7f7d60908eef5e01ca031ab53
Alve Elde <alve_elde@hotmail.com>
2019-07-14 02:14:27 +0200

    Inital project structure

---
+++ uniwagon/__main__.py
@@ -0,0 +1,6 @@
+
+
+if __name__ == '__main__':
+    from uniwagon.wagoncalculator import main
+
+    main(as_module=True)
---
+++ uniwagon/wagoncalculator.py
@@ -0,0 +1,7 @@
+import json
+
+def main(as_module=False):
+    print("Hello World")
+
+if __name__ == "__main__":
+    main(as_module=True)
