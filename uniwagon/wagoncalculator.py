9bb77d0f57c39b5ededa02a373ab12f3c60e4dc4
Alve Elde <alve_elde@hotmail.com>
2019-08-13 13:58:16 +0200

    Introduce ProductTree class

--- uniwagon/wagoncalculator.py
+++ uniwagon/wagoncalculator.py
@@ -291,82 +291,82 @@ class Train:
         print("\n{0:-^{line_len}s}\n".format("", line_len=LINE_LEN))
 
 
-def print_recipe_breakdown(product):
-    if product is None:
-        return
+class ProductTree:
+    def __init__(self, product_name):
+        self.product_dict = {}
+        self.root = self.create_tree(product_name)
 
-    for _component in product.components:
-        pass
+    def create_tree(self, product_name):
+        # Check if the product has already been created.
+        _product = self.product_dict.get(product_name)
+        if _product is not None:
+            return _product
+        _product = Product(product_name)
 
+        # Get the product recipe.
+        _recipe = recipes.get(product_name)
+        if _recipe is None:
+            return None
 
-def create_product_graph(product_name, product_dict):
-    # Check if the product has already been created.
-    _product = product_dict.get(product_name)
-    if _product is not None:
+        # Get the components required for the normal recipe.
+        _components = _recipe.get("recipe")
+        if _components is None:
+            print("File format error: No \"recipe\" key found for: {}".format(product_name))
+            return None
+
+        # Get the product item information.
+        _item = items.get(product_name)
+        if _item is None:
+            return None
+
+        # Get the stack size of the product.
+        _stack_size = _item.get("stack-size")
+        if _stack_size is None:
+            print("File format error: No \"stack-size\" key found for: {}".format(product_name))
+            return None
+
+        # Create and initialize new product.
+        _product.time = _components[0][1]
+        _product.stack_size = _stack_size
+        self.product_dict[product_name] = _product
+
+        # Create Products for each component.
+        for _component in _components[1:]:
+            _new_product = self.create_tree(_component[0])
+            if _new_product is not None:
+                _new_component = Component(_new_product, _component[1])
+                _product.components.append(_new_component)
+
+        # Products with no components are base.
+        if len(_product.components) == 0:
+            _product.is_base = True
         return _product
-    _product = Product(product_name)
 
-    # Get the product recipe.
-    _recipe = recipes.get(product_name)
-    if _recipe is None:
-        return None
-
-    # Get the components required for the normal recipe.
-    _components = _recipe.get("recipe")
-    if _components is None:
-        print("File format error: No \"recipe\" key found for: {}".format(product_name))
-        return None
-
-    # Get the product item information.
-    _item = items.get(product_name)
-    if _item is None:
-        return None
-
-    # Get the stack size of the product.
-    _stack_size = _item.get("stack-size")
-    if _stack_size is None:
-        print("File format error: No \"stack-size\" key found for: {}".format(product_name))
-        return None
-
-    # Create and initialize new product.
-    _product.time = _components[0][1]
-    _product.stack_size = _stack_size
-    product_dict[product_name] = _product
-
-    # Create Products for each component.
-    for _component in _components[1:]:
-        _new_product = create_product_graph(_component[0], product_dict)
-        if _new_product is not None:
-            _new_component = Component(_new_product, _component[1])
-            _product.components.append(_new_component)
-
-    # Products with no components are is_base.
-    if len(_product.components) == 0:
-        _product.is_base = True
-        print(_product.name,"is base")
-    return _product
+    def print(self):
+        pass
 
 
 def main(as_module=False):
     if len(sys.argv) <= 1:
         print("Error: No recipe name provided")
         return
-
-    # Create rooted product graph.
+    
+    # Parse arguments.
     _separator = " "
     _product_name = _separator.join(sys.argv[1:])
-    _product_dict = {}
-    _product = create_product_graph(_product_name, _product_dict)
-    if _product is None :
+    
+    # Create a tree of products with the first argument as root.
+    _product_tree = ProductTree(_product_name)
+    if _product_tree.root is None :
         print("Argument error: No recipe entry found for: {}".format(_product_name))
         return
 
-    print("Recipe created for: ", _product.name)
-    _product.print()
+    
+    _product_tree.root.print()
     _station = Station()
     _station.print()
 
-    _test_train = Train(_product)
+    _test_train = Train(_product_tree.root)
     _test_train.find_max_output()
     _test_train.print()
 
